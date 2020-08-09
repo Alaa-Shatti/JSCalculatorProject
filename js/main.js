1b40349f006ead85edc955e3c9c9fb532fef2315
alaashatti <alaa.shatti@gmail.com>
2020-08-09 12:35:34 +0300

    Modifiled 001

--- js/main.js
+++ js/main.js
@@ -4,12 +4,34 @@ const secondInput = document.getElementById("second-input");
 
 const btnSum = document.getElementById("btn-sum");
 const btnMinus = document.getElementById("btn-minus");
-const btnMultiple = document.getElementById("btn-mutiple");
+const btnMultiple = document.getElementById("btn-multiple");
 const btnDivision = document.getElementById("btn-division");
 const res = document.getElementById("res");
 
 btnSum.addEventListener("click", () => {
   const x = parseFloat(firstInput.value) || 0;
   const y = parseFloat(secondInput.value) || 0;
-  res.innerHTML = (x + y)
+  res.innerHTML = x + y;
+});
+
+btnMinus.addEventListener("click", () => {
+  const x = parseFloat(firstInput.value) || 0;
+  const y = parseFloat(secondInput.value) || 0;
+  res.innerHTML = x - y;
+});
+
+btnMultiple.addEventListener("click", () => {
+  const x = parseFloat(firstInput.value) || 0;
+  const y = parseFloat(secondInput.value) || 0;
+  res.innerHTML = x * y;
+});
+
+btnDivision.addEventListener("click", () => {
+  const x = parseFloat(firstInput.value) || 0;
+  const y = parseFloat(secondInput.value) || 0;
+  if (y === 0) {
+    res.innerHTML = "∞";
+  } else {
+    res.innerHTML = x / y;
+  }
 });
